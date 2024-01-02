c13bc7ce335eabb940a0597434e9eb873d395a2d
ndom91 <yo@ndo.dev>
2024-01-02 21:35:30 +0100

    fix: replace lucide icons with hero-icons

--- src/lib/components/UserSidebar.svelte
+++ src/lib/components/UserSidebar.svelte
@@ -3,7 +3,6 @@
   import * as Tooltip from "$lib/components/ui/tooltip"
   import { Button } from "$lib/components/ui/button"
   import { AvatarMenu } from "$lib/components/navbar"
-  import { Home, Package, Tags } from "lucide-svelte"
   import Logo from "$lib/assets/Logo.svelte"
   import { cn } from "$lib/utils"
   import { useInterface } from "$state/ui.svelte"
@@ -45,7 +44,22 @@
       <a class="flex items-center gap-2 font-semibold" href="/dashboard">
         <Tooltip.Root>
           <Tooltip.Trigger>
-            <Home class="h-6 w-6" />
+            <svg
+              class="size-6"
+              data-slot="icon"
+              fill="none"
+              stroke-width="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              ></path>
+            </svg>
           </Tooltip.Trigger>
           <Tooltip.Content side="right">
             <p>Dashboard</p>
@@ -58,7 +72,22 @@
       <a class="flex items-center gap-2 font-semibold" href="/categories">
         <Tooltip.Root>
           <Tooltip.Trigger>
-            <Package class="h-6 w-6" />
+            <svg
+              class="size-6"
+              data-slot="icon"
+              fill="none"
+              stroke-width="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
+              ></path>
+            </svg>
           </Tooltip.Trigger>
           <Tooltip.Content side="right">
             <p>Categories</p>
@@ -71,7 +100,23 @@
       <a class="flex items-center gap-2 font-semibold" href="/tags">
         <Tooltip.Root>
           <Tooltip.Trigger>
-            <Tags class="h-6 w-6" />
+            <svg
+              class="size-6"
+              data-slot="icon"
+              fill="none"
+              stroke-width="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z"
+              ></path>
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z"></path>
+            </svg>
           </Tooltip.Trigger>
           <Tooltip.Content side="right">
             <p>Tags</p>
--- src/lib/components/navbar/Navbar.svelte
+++ src/lib/components/navbar/Navbar.svelte
@@ -5,7 +5,6 @@
   import * as Tooltip from "$lib/components/ui/tooltip"
   import { QuickAddForm } from "$lib/components/navbar"
   import KeyboardIndicator from "$lib/components/KeyboardIndicator.svelte"
-  import { Plus } from "lucide-svelte"
   import { useInterface } from "$state/ui.svelte"
 
   const ui = useInterface()
@@ -66,7 +65,22 @@
             variant="outline"
             class="size-11 rounded-full p-0"
           >
-            <Plus class="size-5" />
+            <svg
+              class="size-5"
+              data-slot="icon"
+              fill="none"
+              stroke-width="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
+              ></path>
+            </svg>
           </Button>
         </Tooltip.Trigger>
       </Popover.Trigger>
