827efdaa4f939bf1437a8c3ca2fd0d470e323c90
ndom91 <yo@ndo.dev>
2024-01-03 12:37:07 +0100

    fix: breadcrumbs

--- src/lib/components/navbar/Breadcrumbs.svelte
+++ src/lib/components/navbar/Breadcrumbs.svelte
@@ -10,7 +10,6 @@
     // Remove zero-length tokens.
     const tokens = $page.url.pathname.split("/").filter((t) => t !== "")
 
-    // Create { label, href } pairs for each token.
     let tokenPath = ""
     crumbs = tokens.map((t) => {
       tokenPath += "/" + t
@@ -18,7 +17,6 @@
       return { label: t, href: tokenPath }
     })
 
-    // Add a way to get home too.
     crumbs.unshift({ label: "Home", href: "/dashboard" })
   }
 </script>
@@ -30,19 +28,7 @@
         {c.label}
       </span>
     {:else}
-      <a href={c.href}>{c.label}</a>
-      <svg
-        class="size-4"
-        data-slot="icon"
-        fill="none"
-        stroke-width="1.5"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-      >
-        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
-      </svg>
+      <a href={c.href}>{c.label}</a>&nbsp;/&nbsp;
     {/if}
   {/each}
 </div>
