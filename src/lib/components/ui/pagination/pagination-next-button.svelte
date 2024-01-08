c2ec3b3a47b8e403a1ae8f1ae54f221a4bdb6704
ndom91 <yo@ndo.dev>
2024-01-08 13:45:23 +0100

    fix: pagination chevron icons

--- src/lib/components/ui/pagination/pagination-next-button.svelte
+++ src/lib/components/ui/pagination/pagination-next-button.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
-	import { Pagination as PaginationPrimitive } from "bits-ui";
-	import { Button } from "$lib/components/ui/button";
-	import { cn } from "$lib/utils";
-	import { ChevronRight } from "lucide-svelte";
+  import { Pagination as PaginationPrimitive } from "bits-ui"
+  import { Button } from "$lib/components/ui/button"
+  import { cn } from "$lib/utils"
 
-	type $$Props = PaginationPrimitive.NextButtonProps;
-	type $$Events = PaginationPrimitive.NextButtonEvents;
+  type $$Props = PaginationPrimitive.NextButtonProps
+  type $$Events = PaginationPrimitive.NextButtonEvents
 
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+  let className: $$Props["class"] = undefined
+  export { className as class }
 </script>
 
 <PaginationPrimitive.NextButton asChild let:builder>
-	<Button
-		variant="ghost"
-		class={cn("gap-1 pl-2.5", className)}
-		builders={[builder]}
-		on:click
-		{...$$restProps}
-	>
-		<span>Next</span>
-		<ChevronRight class="h-4 w-4" />
-	</Button>
+  <Button
+    variant="ghost"
+    class={cn("flex items-center justify-between gap-1 px-2.5", className)}
+    builders={[builder]}
+    on:click
+    {...$$restProps}
+  >
+    <span>Next</span>
+    <svg
+      class="size-4"
+      data-slot="icon"
+      fill="none"
+      stroke-width="1.5"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
+    </svg>
+  </Button>
 </PaginationPrimitive.NextButton>
--- src/lib/components/ui/pagination/pagination-prev-button.svelte
+++ src/lib/components/ui/pagination/pagination-prev-button.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
-	import { Pagination as PaginationPrimitive } from "bits-ui";
-	import { Button } from "$lib/components/ui/button";
-	import { cn } from "$lib/utils";
-	import { ChevronLeft } from "lucide-svelte";
+  import { Pagination as PaginationPrimitive } from "bits-ui"
+  import { Button } from "$lib/components/ui/button"
+  import { cn } from "$lib/utils"
 
-	type $$Props = PaginationPrimitive.PrevButtonProps;
-	type $$Events = PaginationPrimitive.PrevButtonEvents;
+  type $$Props = PaginationPrimitive.PrevButtonProps
+  type $$Events = PaginationPrimitive.PrevButtonEvents
 
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+  let className: $$Props["class"] = undefined
+  export { className as class }
 </script>
 
 <PaginationPrimitive.PrevButton asChild let:builder>
-	<Button
-		variant="ghost"
-		class={cn("gap-1 pl-2.5", className)}
-		builders={[builder]}
-		on:click
-		{...$$restProps}
-	>
-		<ChevronLeft class="h-4 w-4" />
-		<span>Previous</span>
-	</Button>
+  <Button
+    variant="ghost"
+    class={cn("flex items-center justify-between gap-1 px-2.5", className)}
+    builders={[builder]}
+    on:click
+    {...$$restProps}
+  >
+    <svg
+      class="size-4"
+      data-slot="icon"
+      fill="none"
+      stroke-width="1.5"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5"></path>
+    </svg>
+    <span>Previous</span>
+  </Button>
 </PaginationPrimitive.PrevButton>
