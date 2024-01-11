be9ea68417d8c05919f68f446cf74c51eaf6ea9d
ndom91 <yo@ndo.dev>
2024-01-11 16:48:16 +0100

    feat: setup initial settings page

---
+++ src/routes/settings/+page.server.ts
@@ -0,0 +1,108 @@
+import prisma from "$lib/prisma";
+import { fail } from "@sveltejs/kit";
+import type { Actions } from "./$types"
+import type { PageServerLoad } from "./$types";
+import { WORKER_URL } from '$env/static/private';
+
+export const actions: Actions = {
+  deleteFeed: async ({ request, locals }) => {
+    const session = await locals.getSession()
+    if (!session?.user?.userId) {
+      return fail(401, { type: "error", error: "Unauthenticated" })
+    }
+    const data = await request.formData();
+    const feedId = String(data.get('feedId'))
+
+    if (!feedId) {
+      return fail(400, { type: "error", message: "Missing Feed ID" })
+    }
+
+    await prisma.feed.delete({
+      where: {
+        id: feedId,
+        userId: session.user.userId,
+      }
+    });
+    return { type: "success", message: 'Deleted Feed' }
+  },
+  addFeed: async ({ request, locals }) => {
+    if (!WORKER_URL) {
+      return fail(500, { type: "error", error: "Worker URL Not Configured!" })
+    }
+    const session = await locals.getSession()
+    if (!session?.user?.userId) {
+      return fail(401, { type: "error", error: "Unauthenticated" })
+    }
+    const data = await request.formData();
+    const feedUrl = String(data.get('feedUrl'))
+
+    if (!feedUrl) {
+      return fail(400, { type: "error", message: "Feed URL Required" })
+    }
+
+    await fetch(`${WORKER_URL}/feed`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        feedUrl,
+        userId: session.user.userId,
+      }),
+    })
+
+    return { type: "success", message: 'Added Feed' }
+  },
+}
+
+export const load: PageServerLoad = async ({ parent, locals, url }) => {
+  await parent()
+  try {
+    const session = await locals.getSession();
+    if (!session?.user?.userId) {
+      return fail(401, { type: "error", error: "Unauthenticated" })
+    }
+    const skip = Number(url.searchParams.get('skip') ?? "0")
+    const limit = Number(url.searchParams.get('limit') ?? "10")
+
+    if (limit > 100) {
+      return fail(401, { type: "error", error: "Attempted to load too many items" })
+    }
+
+    const [feedData, feedCount] = await prisma.feed.findManyAndCount({
+      where: { userId: session?.user?.userId },
+      select: {
+        id: true,
+        name: true,
+        url: true,
+        description: true,
+        language: true,
+        userId: true,
+        lastFetched: true,
+        createdAt: true,
+        updatedAt: true,
+        _count: {
+          select: {
+            feedEntries: { where: { unread: true } },
+          },
+        },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return {
+      feeds: {
+        data: feedData,
+        count: feedCount
+      }
+    };
+  } catch (error) {
+    let message
+    if (typeof error === "string") {
+      message = error
+    } else if (error instanceof Error) {
+      message = error.message
+    }
+    return { feedEntries: [], count: 0, error: message };
+  }
+};
---
+++ src/routes/settings/components/DeleteDialog.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import type { ActionData } from "./$types"
+  import { enhance } from "$app/forms"
+  import { form_action } from "$lib/form_action"
+  import { buttonVariants } from "$lib/components/ui/button"
+  import * as AlertDialog from "$lib/components/ui/alert-dialog"
+  import type { Feed } from "$zod"
+
+  let { form, open, feed } = $props<{
+    form?: ActionData
+    open: boolean
+    feed: Feed
+  }>()
+
+  $effect(() => {
+    if ($form?.type === "success") {
+      open = false
+    }
+  })
+</script>
+
+<AlertDialog.Root bind:open closeOnOutsideClick closeOnEscape>
+  <AlertDialog.Content>
+    <AlertDialog.Header>
+      <AlertDialog.Title>Are you sure?</AlertDialog.Title>
+      <AlertDialog.Description>
+        This action cannot be undone. This will permanently delete your feed from <span
+          class="font-bold">{new URL(feed.url).host}</span
+        > and remove any related data from our servers.
+      </AlertDialog.Description>
+    </AlertDialog.Header>
+    <AlertDialog.Footer>
+      <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+      <AlertDialog.Action asChild>
+        <form action="?/deleteFeed" method="post" use:enhance={form_action()}>
+          <input type="hidden" name="feedId" value={feed.id} />
+          <button class={buttonVariants({ variant: "destructive" })} type="submit">
+            Continue
+          </button>
+        </form>
+      </AlertDialog.Action>
+    </AlertDialog.Footer>
+  </AlertDialog.Content>
+</AlertDialog.Root>
