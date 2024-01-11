975826ad1c82d9ac4100d4b39e9a264f637c9849
ndom91 <yo@ndo.dev>
2024-01-11 10:49:25 +0100

    feat: add keyboard shortcuts help modal

---
+++ src/lib/components/KeyboardShortcutsHelp.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { default as Kbd } from "$lib/components/KeyboardIndicator.svelte"
+  import * as Dialog from "$lib/components/ui/dialog"
+
+  export let open = false
+</script>
+
+<Dialog.Root bind:open>
+  <Dialog.Content>
+    <Dialog.Header>
+      <Dialog.Title>Keyboard Shortcuts</Dialog.Title>
+      <Dialog.Description>
+        <table class="my-4 w-full">
+          <tbody class="*:h-10">
+            <tr>
+              <td>Show Shortcut Help</td>
+              <td><Kbd key="Ctrl + ?" /></td>
+            </tr>
+            <tr>
+              <td>Toggle Right Sidebar</td>
+              <td><Kbd key="]" /></td>
+            </tr>
+            <tr>
+              <td>Toggle Left Sidebar</td>
+              <td><Kbd key="[" /></td>
+            </tr>
+            <tr>
+              <td>Focus element in list down</td>
+              <td><Kbd key="j" /> / <Kbd key="ArrowDown" /></td>
+            </tr>
+            <tr>
+              <td>Focus element in list up</td>
+              <td><Kbd key="k" /> / <Kbd key="ArrowUp" /></td>
+            </tr>
+            <tr>
+              <td>Open RSS article to read</td>
+              <td><Kbd key="\\" /></td>
+            </tr>
+            <tr>
+              <td>Focus search input</td>
+              <td><Kbd key="/" /></td>
+            </tr>
+          </tbody>
+        </table>
+      </Dialog.Description>
+    </Dialog.Header>
+  </Dialog.Content>
+</Dialog.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Toaster } from "svelte-french-toast"
   import { onNavigate } from "$app/navigation"
+  import KeyboardShortcutsHelp from "$lib/components/KeyboardShortcutsHelp.svelte"
   import "$lib/styles/style.css"
 
   // View transition
@@ -14,6 +15,16 @@
       })
     })
   })
+
+  let showKeyboardShortcuts = $state(false)
+
+  const handleKeyDown = (e) => {
+    if (e.repeat || e.target instanceof HTMLInputElement) return
+    if ((e.ctrlKey || e.metaKey) && e.key === "/") {
+      e.preventDefault()
+      showKeyboardShortcuts = !showKeyboardShortcuts
+    }
+  }
 </script>
 
 <svelte:head>
@@ -21,5 +32,10 @@
   <meta name="description" content="This is where the description goes for SEO" />
 </svelte:head>
 
+<svelte:window onkeydown={handleKeyDown} />
+
+{#if showKeyboardShortcuts}
+  <KeyboardShortcutsHelp bind:open={showKeyboardShortcuts} />
+{/if}
 <Toaster />
 <slot />
