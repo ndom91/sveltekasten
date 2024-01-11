c2c4f2dc39d76987aa78b6727efdea544f9adc86
ndom91 <yo@ndo.dev>
2024-01-11 14:18:07 +0100

    feat: update feedActions btns

--- src/lib/components/feed-row/FeedActions.svelte
+++ src/lib/components/feed-row/FeedActions.svelte
@@ -6,6 +6,7 @@
   export let url: string
   export let handleMetadataSidebarOpen: () => void
   export let toggleCardOpen: () => void
+  export let handleMarkAsUnread: (target?: boolean) => void
 </script>
 
 <div
@@ -14,25 +15,7 @@
     isOptionsOpen ? "opacity-100" : "opacity-0",
   )}
 >
-  <Button variant="ghost" size="icon" href={url} target="_blank">
-    <svg
-      class="size-5 text-zinc-900 dark:text-zinc-100"
-      data-slot="icon"
-      fill="none"
-      stroke-width="1.5"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
-      ></path>
-    </svg>
-  </Button>
-  <Button variant="ghost" size="icon" on:click={toggleCardOpen}>
+  <Button title="Read" variant="ghost" size="icon" on:click={toggleCardOpen}>
     <svg
       class="size-5 text-zinc-900 dark:text-zinc-100"
       data-slot="icon"
@@ -50,7 +33,7 @@
       ></path>
     </svg>
   </Button>
-  <Button variant="ghost" size="icon" on:click={handleMetadataSidebarOpen}>
+  <Button title="Mark as Unread" variant="ghost" size="icon" on:click={() => handleMarkAsUnread()}>
     <svg
       class="size-5 text-zinc-900 dark:text-zinc-100"
       data-slot="icon"
@@ -64,8 +47,44 @@
       <path
         stroke-linecap="round"
         stroke-linejoin="round"
-        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+        d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z"
       ></path>
     </svg>
   </Button>
+  <Button title="Open Link" variant="ghost" size="icon" href={url} target="_blank">
+    <svg
+      class="size-5 text-zinc-900 dark:text-zinc-100"
+      data-slot="icon"
+      fill="none"
+      stroke-width="1.5"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+      ></path>
+    </svg>
+  </Button>
+  <!-- <Button variant="ghost" size="icon" on:click={handleMetadataSidebarOpen}> -->
+  <!--   <svg -->
+  <!--     class="size-5 text-zinc-900 dark:text-zinc-100" -->
+  <!--     data-slot="icon" -->
+  <!--     fill="none" -->
+  <!--     stroke-width="1.5" -->
+  <!--     stroke="currentColor" -->
+  <!--     viewBox="0 0 24 24" -->
+  <!--     xmlns="http://www.w3.org/2000/svg" -->
+  <!--     aria-hidden="true" -->
+  <!--   > -->
+  <!--     <path -->
+  <!--       stroke-linecap="round" -->
+  <!--       stroke-linejoin="round" -->
+  <!--       d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" -->
+  <!--     ></path> -->
+  <!--   </svg> -->
+  <!-- </Button> -->
 </div>
