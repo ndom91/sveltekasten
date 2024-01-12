fb31a8fe3916098142e7e677484f002af4c27f67
ndom91 <yo@ndo.dev>
2024-01-12 19:25:20 +0100

    fix: prisma extend typescript

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,18 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient().$extends({
+// Prisma workaround: https://github.com/prisma/prisma/discussions/3087
+const findManyAndCount = {
   name: 'findManyAndCount',
   model: {
     $allModels: {
       findManyAndCount<Model, Args>(
         this: Model,
         args: Prisma.Exact<Args, Prisma.Args<Model, 'findMany'>>
-      ): Promise<[Prisma.Result<Model, Args, 'findMany'>, number]> {
+      ): Promise<[
+        Prisma.Result<Model, Args, 'findMany'>,
+        number,
+        Args extends { take: number } ? number : undefined
+      ]> {
         return prisma.$transaction([
           (this as any).findMany(args),
           (this as any).count({ where: (args as any).where })
@@ -15,6 +20,15 @@ const prisma = new PrismaClient().$extends({
       }
     }
   }
-});
+}
+
+function createExtendedClient() {
+  return new PrismaClient().$extends(findManyAndCount);
+}
+
+type ExtendedPrismaClient = ReturnType<typeof createExtendedClient>;
+type PC = Omit<ExtendedPrismaClient, keyof PrismaClient> & PrismaClient
+
+const prisma = createExtendedClient() as unknown as PC;
 
 export default prisma
