402a3bed45199cf057fb4603301eae1d5b056446
ndom91 <yo@ndo.dev>
2024-01-15 17:56:57 +0100

    feat: emptystate snippet on bookmark page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -135,27 +135,7 @@
           {#each bookmarks as bookmark}
             <BookmarkRow {bookmark} />
           {:else}
-            <EmptyState />
-            <p class="mx-auto w-1/2 text-center text-muted-foreground">
-              Get started by adding a bookmark with the
-              <svg
-                class="inline p-1 rounded-md size-7 dark:bg-zinc-700"
-                data-slot="icon"
-                fill="none"
-                stroke-width="1.5"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
-                ></path>
-              </svg>
-              button above or by pressing <KeyboardIndicator class="text-sm" key="Alt N" />
-            </p>
+            {@render emptyHelper()}
           {/each}
           <div bind:this={elementRef} class="w-full h-24" />
         {:catch error}
@@ -165,27 +145,31 @@
         {/await}
       </div>
     {:else}
-      <EmptyState />
-      <p class="mx-auto w-1/2 text-center text-muted-foreground">
-        Get started by adding a bookmark with the
-        <svg
-          class="inline p-1 rounded-md size-7 dark:bg-zinc-700"
-          data-slot="icon"
-          fill="none"
-          stroke-width="1.5"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
-          ></path>
-        </svg>
-        button above or by pressing <KeyboardIndicator class="text-sm" key="Alt N" />
-      </p>
+      {@render emptyHelper()}
     {/if}
   </div>
 </main>
+
+{#snippet emptyHelper()}
+  <EmptyState />
+  <p class="mx-auto w-1/2 text-center text-muted-foreground">
+    Get started by adding a bookmark with the
+    <svg
+      class="inline p-1 rounded-md size-7 dark:bg-zinc-700"
+      data-slot="icon"
+      fill="none"
+      stroke-width="1.5"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z"
+      ></path>
+    </svg>
+    button above or by pressing <KeyboardIndicator class="text-sm" key="Alt N" />
+  </p>
+{/snippet}
