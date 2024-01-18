578763f323625d86539fd9b5b7bcf9e24bcf245c
ndom91 <yo@ndo.dev>
2024-01-18 19:42:48 +0100

    fix: svelte plugin not matching vite plugins[] type

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  // @ts-expect-error
   plugins: [sveltekit()],
   host: "0.0.0.0",
 })
