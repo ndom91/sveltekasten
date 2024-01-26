a0bf787ec808f803ef55c9081865a0c25c01f5f7
ndom91 <yo@ndo.dev>
2024-01-26 22:52:13 +0100

    fix: cleanup vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  // @ts-expect-error
   plugins: [sveltekit()],
-  host: "0.0.0.0",
+  server: {
+    host: "0.0.0.0",
+  },
 })
