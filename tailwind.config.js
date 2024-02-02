e0a6738af0fa516ace56e32e2d9b908040310002
ndom91 <yo@ndo.dev>
2024-02-02 12:40:40 +0100

    fix: add fluid typescale to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -11,12 +11,24 @@ const config = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px"
-      }
+        "2xl": "1400px",
+      },
+    },
+    fontSize: {
+      xs: "clamp(0.64rem, 0.05vw + 0.63rem, 0.67rem)",
+      sm: "clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem)",
+      base: "clamp(1rem, 0.34vw + 0.91rem, 1.19rem)",
+      lg: "clamp(1.25rem, 0.61vw + 1.1rem, 1.58rem)",
+      xl: "clamp(1.56rem, 1vw + 1.31rem, 2.11rem)",
+      "2xl": "clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)",
+      "3xl": "clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)",
+      "4xl": "clamp(3.05rem, 3.54vw + 2.17rem, 5rem)",
+      "5xl": "clamp(3.81rem, 5.18vw + 2.52rem, 6.66rem)",
+      "6xl": "clamp(4.77rem, 7.48vw + 2.9rem, 8.88rem)",
     },
     extend: {
       transitionProperty: {
-        width: "width"
+        width: "width",
       },
       colors: {
         border: "hsl(var(--border) / <alpha-value>)",
@@ -26,43 +38,43 @@ const config = {
         foreground: "hsl(var(--foreground) / <alpha-value>)",
         primary: {
           DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-          foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+          foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-          foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+          foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
         },
         muted: {
           DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-          foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+          foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
         },
         accent: {
           DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-          foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+          foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
         },
         popover: {
           DEFAULT: "hsl(var(--popover) / <alpha-value>)",
-          foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+          foreground: "hsl(var(--popover-foreground) / <alpha-value>)",
         },
         card: {
           DEFAULT: "hsl(var(--card) / <alpha-value>)",
-          foreground: "hsl(var(--card-foreground) / <alpha-value>)"
-        }
+          foreground: "hsl(var(--card-foreground) / <alpha-value>)",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)"
+        sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: [...fontFamily.sans]
-      }
-    }
+        sans: [...fontFamily.sans],
+      },
+    },
   },
-};
+}
 
-export default config;
+export default config
