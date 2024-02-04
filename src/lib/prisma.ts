936e5e1b237e11d6105aa8959d7b84244412bb65
ndom91 <yo@ndo.dev>
2024-02-04 22:46:12 +0100

    fix: prisma types and extend updates

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,41 @@
 import { Prisma, PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient().$extends({
-  name: "findManyAndCount",
-  model: {
-    $allModels: {
-      findManyAndCount<Model, Args>(
-        this: Model,
-        args: Prisma.Exact<Args, Prisma.Args<Model, "findMany">>,
-      ): Promise<[Prisma.Result<Model, Args, "findMany">, number]> {
-        return prisma.$transaction([
-          (this as any).findMany(args),
-          (this as any).count({ where: (args as any).where }),
-        ]) as any
+const prisma = new PrismaClient()
+  .$extends({
+    name: "findManyAndCount",
+    model: {
+      $allModels: {
+        findManyAndCount<Model, Args>(
+          this: Model,
+          args: Prisma.Exact<Args, Prisma.Args<Model, "findMany">>,
+        ): Promise<[Prisma.Result<Model, Args, "findMany">, number]> {
+          return prisma.$transaction([
+            (this as any).findMany(args),
+            (this as any).count({ where: (args as any).where }),
+          ]) as any
+        },
       },
     },
-  },
-})
+  })
+  .$extends({
+    result: {
+      bookmark: {
+        save: {
+          needs: { id: true },
+          compute(bookmark) {
+            return () => prisma.bookmark.update({ where: { id: bookmark.id }, data: bookmark })
+          },
+        },
+      },
+      user: {
+        save: {
+          needs: { id: true },
+          compute(user) {
+            return () => prisma.user.update({ where: { id: user.id }, data: user })
+          },
+        },
+      },
+    },
+  })
 
 export default prisma
