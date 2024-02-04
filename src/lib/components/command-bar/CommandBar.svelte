049b79339eb27c636cbaca1f3ef73c68bbb7a8ac
ndom91 <yo@ndo.dev>
2024-02-04 22:45:29 +0100

    feat: update Commandbar options

--- src/lib/components/command-bar/CommandBar.svelte
+++ src/lib/components/command-bar/CommandBar.svelte
@@ -33,26 +33,33 @@
   <Command.Input type="command-input" placeholder="Type a command or search..." />
   <Command.List>
     <Command.Empty>No results found.</Command.Empty>
+    <Command.Group heading="Actions">
+      <Command.Item onSelect={toggleDarkMode}>Toggle Dark Mode</Command.Item>
+      <Command.Item onSelect={openQuickAdd}>New Bookmark</Command.Item>
+    </Command.Group>
+    <Command.Separator />
     <Command.Group heading="Pages">
       <Command.Item onSelect={() => goto("/dashboard")} class="flex justify-between">
         Dashboard <KeyboardIndicator class="text-xs" key="Shift + 1" />
       </Command.Item>
+      <Command.Item onSelect={() => goto("/dashboard/bookmarks")} class="flex justify-between">
+        Bookmarks <KeyboardIndicator class="text-xs" key="Shift + 2" />
+      </Command.Item>
       <Command.Item onSelect={() => goto("/dashboard/feeds")} class="flex justify-between">
-        Feeds <KeyboardIndicator class="text-xs" key="Shift + 2" />
+        Feeds <KeyboardIndicator class="text-xs" key="Shift + 3" />
+      </Command.Item>
+      <Command.Item onSelect={() => goto("/dashboard/archives")} class="flex justify-between">
+        Archive <KeyboardIndicator class="text-xs" key="Shift + 4" />
       </Command.Item>
       <Command.Item onSelect={() => goto("/dashboard/categories")} class="flex justify-between">
-        Categories <KeyboardIndicator class="text-xs" key="Shift + 3" />
+        Categories <KeyboardIndicator class="text-xs" key="Shift + 5" />
       </Command.Item>
       <Command.Item onSelect={() => goto("/dashboard/tags")} class="flex justify-between">
-        Tags <KeyboardIndicator class="text-xs" key="Shift + 4" />
+        Tags <KeyboardIndicator class="text-xs" key="Shift + 6" />
       </Command.Item>
-    </Command.Group>
-    <Command.Separator />
-    <Command.Group heading="Actions">
-      <Command.Item onSelect={toggleDarkMode}>Toggle Dark Mode</Command.Item>
-      <Command.Item onSelect={openQuickAdd}>New Bookmark</Command.Item>
-      <Command.Item>Delete Bookmark</Command.Item>
-      <Command.Item>Edit Bookmark</Command.Item>
+      <Command.Item onSelect={() => goto("/settings")} class="flex justify-between">
+        Settings <KeyboardIndicator class="text-xs" key="Shift + 7" />
+      </Command.Item>
     </Command.Group>
   </Command.List>
 </Command.Dialog>
