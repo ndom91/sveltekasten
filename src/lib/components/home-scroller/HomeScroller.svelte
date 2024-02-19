62fbebb982b7d58dacbb1810d4bd7a0dc5088d86
ndom91 <yo@ndo.dev>
2024-02-19 16:08:08 +0100

    feat: homescroller style updates

--- src/lib/components/home-scroller/HomeScroller.svelte
+++ src/lib/components/home-scroller/HomeScroller.svelte
@@ -11,15 +11,80 @@
   const { type, items, count } = $props<ScrollerProps>()
 </script>
 
-<section class="flex flex-col mx-4 max-w-full rounded-lg bg-zinc-900">
-  <div class="flex flex-grow justify-between mx-4 mt-4">
+<section
+  class="relative flex flex-col mx-4 max-w-full rounded-lg bg-zinc-900 overflow-hidden after:pointer-events-none after:absolute after:bottom-0 after:right-0 after:h-96 after:w-24 after:shadow-[inset_-100px_0px_35px_-55px_#141414]"
+>
+  <div class="flex z-10 flex-grow justify-between mx-4 mt-4 mr-8">
     <h2 class="text-2xl font-thin">
       {type === "feeds"
         ? `Unread ${type.charAt(0).toUpperCase() + type.slice(1)}`
         : type.charAt(0).toUpperCase() + type.slice(1)}
     </h2>
     {#if items.length}
-      <a data-sveltekit-preload-data="hover" href={`/${type}`}>See more</a>
+      <a class="flex gap-2 items-center" data-sveltekit-preload-data="hover" href={`/${type}`}>
+        <svg
+          class="size-5 text-neutral-600 dark:text-neutral-500"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 256"
+        >
+          <rect width="256" height="256" fill="none" />
+          <line
+            x1="40"
+            y1="64"
+            x2="216"
+            y2="64"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          />
+          <line
+            x1="40"
+            y1="128"
+            x2="112"
+            y2="128"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          />
+          <line
+            x1="40"
+            y1="192"
+            x2="128"
+            y2="192"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          />
+          <circle
+            cx="184"
+            cy="144"
+            r="32"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          />
+          <line
+            x1="206.63"
+            y1="166.63"
+            x2="232"
+            y2="192"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          />
+        </svg>
+        <span class="text-muted">See more</span>
+      </a>
     {/if}
   </div>
   <div class="flex overflow-x-auto">
