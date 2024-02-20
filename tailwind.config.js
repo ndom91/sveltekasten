437570cce4bfad19842d2d313c9a4e3ab6612d05
ndom91 <yo@ndo.dev>
2024-02-20 23:05:01 +0100

    chore: add 3xl drop-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,15 @@ const config = {
       lg: StaticShadows["--shadow-4"],
       xl: StaticShadows["--shadow-5"],
       "2xl": StaticShadows["--shadow-6"],
+      "3xl": [
+        "0px 0px 7.6px rgba(0, 0, 0, 0.046)",
+        "0px 0px 17.6px rgba(0, 0, 0, 0.067)",
+        "0px 0px 31.5px rgba(0, 0, 0, 0.083)",
+        "0px 0px 52.3px rgba(0, 0, 0, 0.097)",
+        "0px 0px 86.2px rgba(0, 0, 0, 0.113)",
+        "0px 0px 150.7px rgba(0, 0, 0, 0.134)",
+        "0px 0px 326px rgba(0, 0, 0, 0.18)",
+      ],
     },
     extend: {
       transitionProperty: {
