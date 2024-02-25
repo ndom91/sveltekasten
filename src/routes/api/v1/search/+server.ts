1566296b1beae04fc7976f3ce02b3fec10821fcd
ndom91 <yo@ndo.dev>
2024-02-25 12:12:01 +0100

    fix: upgarde /api/v1/search handler

--- src/routes/api/v1/search/+server.ts
+++ src/routes/api/v1/search/+server.ts
@@ -7,29 +7,58 @@ type RequestBody = {
   include: Record<string, unknown>
   orderBy: Record<string, Record<string, string>>
   type: "feedEntry" | "bookmark"
+  limit: number
+  skip: number
 }
 
 export const POST: RequestHandler = async ({ request, locals, url }) => {
   try {
     const session = await locals.auth()
-    if (!session?.user?.userId) {
+    if (!session?.user?.id) {
       return fail(401, { type: "error", error: "Unauthenticated" })
     }
 
     let returnData
-    const skip = Number(url.searchParams.get("skip") ?? "0")
-    const limit = Number(url.searchParams.get("limit") ?? "10")
-    const { where, include, orderBy, type } = (await request.json()) as RequestBody
+    // const skip = Number(url.searchParams.get("skip") ?? "0")
+    // const limit = Number(url.searchParams.get("limit") ?? "10")
+    console.log("v1/search POST handler")
+    const {
+      include,
+      orderBy,
+      type,
+      where = {},
+      limit = 20,
+      skip = 0,
+    } = (await request.json()) as RequestBody
     if (!type) {
       return fail(401, { type: "error", error: "Missing 'type' parameter" })
     }
 
+    console.log("loading more..", { skip, limit })
+    console.log(
+      "findManyAndCount.args",
+      JSON.stringify(
+        {
+          take: limit,
+          skip: skip,
+          where: {
+            ...where,
+            userId: session?.user?.id,
+          },
+          include,
+          orderBy,
+        },
+        null,
+        2,
+      ),
+    )
+
     const [data, count] = await prisma[type].findManyAndCount({
-      take: limit + skip,
+      take: limit,
       skip: skip,
       where: {
-        userId: session?.user?.userId,
         ...where,
+        userId: session?.user?.id,
       },
       include,
       orderBy,
@@ -43,6 +72,7 @@ export const POST: RequestHandler = async ({ request, locals, url }) => {
       returnData = data
     }
 
+    console.log("findManyAndCount.length", data.length)
     return json({ data: returnData, count })
   } catch (error: any) {
     console.error("Search Error", error)
