ad3db15b279ee31df62a9f9880c672272727fe2b
ndom91 <yo@ndo.dev>
2024-03-06 22:59:55 +0100

    feat: add drag-and-drop and paste-into bookmark adding mechanisms

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { z } from "zod"
   import { Toaster } from "svelte-french-toast"
   // import { Toaster } from "$lib/components/ui/sonner"
+  import toast from "svelte-french-toast"
+  import { cn } from "$lib/utils/style"
   import { goto, onNavigate } from "$app/navigation"
+  import ConfirmAddDialog from "./ConfirmAddDialog.svelte"
   import KeyboardShortcutsHelp from "$lib/components/KeyboardShortcutsHelp.svelte"
   import "$lib/styles/global.css"
 
@@ -56,6 +60,57 @@
       goto("/settings")
     }
   }
+
+  const parsePassedData = (text: string | undefined): string | void => {
+    try {
+      const rawUrl = z.string().url().parse(text)
+      return rawUrl
+    } catch (error) {
+      console.error(`Got invalid paste content: "${text}"`)
+      toast.error("Paste failed - invalid URL")
+    }
+  }
+
+  let isDragOver = $state(false)
+  let showConfirmAddDialog = $state(false)
+  let url = $state("")
+
+  $effect(() => {
+    if (isDragOver) {
+      setTimeout(() => {
+        isDragOver = false
+      }, 10000)
+    }
+  })
+
+  const handleDragEnter = (e: DragEvent) => {
+    e.preventDefault()
+    e.stopPropagation()
+    isDragOver = true
+  }
+
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault()
+    e.stopPropagation()
+    isDragOver = false
+
+    const text = e.dataTransfer?.getData("text/plain")
+    const parsedUrl = parsePassedData(text)
+    if (parsedUrl) {
+      url = parsedUrl
+      showConfirmAddDialog = true
+    }
+  }
+
+  const handlePaste = (e: ClipboardEvent) => {
+    if (e.target instanceof HTMLInputElement) return
+    const text = e.clipboardData?.getData("text/plain")
+    const parsedUrl = parsePassedData(text)
+    if (parsedUrl) {
+      url = parsedUrl
+      showConfirmAddDialog = true
+    }
+  }
 </script>
 
 <svelte:head>
@@ -63,13 +118,41 @@
   <meta name="description" content="RSS Feeds, Bookmarks and more!" />
 </svelte:head>
 
-<svelte:window onkeydown={handleKeyDown} />
+<svelte:window
+  onkeydown={handleKeyDown}
+  onpaste={handlePaste}
+  ondragenter={handleDragEnter}
+  ondragover={(e) => e.preventDefault()}
+/>
+
+<ConfirmAddDialog {url} bind:open={showConfirmAddDialog} />
 
 {#if showKeyboardShortcuts}
   <KeyboardShortcutsHelp bind:open={showKeyboardShortcuts} />
 {/if}
+
 <Toaster
   position="bottom-right"
   toastOptions={{ style: "background-color: #111111; color: #fff" }}
 />
+
 <slot />
+
+<div
+  role="region"
+  aria-hidden="true"
+  class={cn(
+    "grid fixed inset-0 z-50 place-items-center bg-black transition duration-500 bg-opacity-30 backdrop-blur-sm",
+    isDragOver ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0",
+  )}
+>
+  <div
+    role="region"
+    aria-hidden="true"
+    class="w-[95%] h-[95%] border-4 border-dashed border-white border-opacity-50 grid place-items-center rounded-lg"
+    ondragover={(e) => e.preventDefault()}
+    ondrop={handleDrop}
+  >
+    <span class="text-2xl">Drop Here</span>
+  </div>
+</div>
---
+++ src/routes/ConfirmAddDialog.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { page } from "$app/stores"
+  import toast from "svelte-french-toast"
+  import { invalidate } from "$app/navigation"
+  import { buttonVariants } from "$lib/components/ui/button"
+  import * as AlertDialog from "$lib/components/ui/alert-dialog"
+
+  let { open, url } = $props<{
+    open: boolean
+    url: string
+  }>()
+
+  const handleConfirm = async (): Promise<void> => {
+    try {
+      const addResponse = await fetch("/api/v1/bookmarks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([
+          {
+            url,
+            userId: $page.data.session?.user?.id,
+          },
+        ]),
+      })
+      if (addResponse.ok) {
+        toast.success(`Added "${url}"`)
+        invalidate("app:bookmarks")
+      }
+      return
+    } catch (error) {
+      console.error(error)
+      toast.error(String(error))
+    } finally {
+      open = false
+    }
+  }
+</script>
+
+<AlertDialog.Root bind:open closeOnOutsideClick closeOnEscape>
+  <AlertDialog.Content>
+    <AlertDialog.Header>
+      <AlertDialog.Title>Add New Bookmark</AlertDialog.Title>
+      <AlertDialog.Description>
+        <div>Are you sure you want to add this URL?</div>
+        <div class="mt-2 font-bold">{url}?</div>
+      </AlertDialog.Description>
+    </AlertDialog.Header>
+    <AlertDialog.Footer>
+      <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+      <AlertDialog.Action asChild>
+        <button class={buttonVariants({ variant: "default" })} onclick={handleConfirm}>
+          Add
+        </button>
+      </AlertDialog.Action>
+    </AlertDialog.Footer>
+  </AlertDialog.Content>
+</AlertDialog.Root>
