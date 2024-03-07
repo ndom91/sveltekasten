d67eb4443ad850cb4631f7b70f1ce61e7fbb3eaf
ndom91 <yo@ndo.dev>
2024-03-07 19:44:22 +0100

    fix: add settings tab queryParam sync

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { page } from "$app/stores"
+  import { goto } from "$app/navigation"
   import Sidebar from "$lib/components/UserSidebar.svelte"
   import UserSection from "./components/UserSection.svelte"
   import FeedsSection from "./components/FeedsSection.svelte"
@@ -6,6 +8,12 @@
   import { Navbar } from "$lib/components/navbar"
   import { CommandBar } from "$lib/components/command-bar"
   import * as Tabs from "$lib/components/ui/tabs"
+
+  const tab = $derived($page.url.searchParams.get("tab"))
+
+  const handleTabChange = (data) => {
+    goto(`?tab=${data}`)
+  }
 </script>
 
 <svelte:head>
@@ -18,16 +26,18 @@
   <Sidebar />
   <div class="flex flex-col flex-grow items-center w-full">
     <Navbar simple />
-    <Tabs.Root value="user" class="flex flex-col flex-grow w-full max-w-screen-2xl">
+    <Tabs.Root
+      value={tab ?? "user"}
+      class="flex flex-col flex-grow w-full max-w-screen-2xl"
+      onValueChange={handleTabChange}
+    >
       <div class="p-4">
         <Tabs.List
           class="*:w-full *:text-lg *:font-normal h-12 w-full bg-zinc-100 dark:bg-zinc-800"
         >
           <Tabs.Trigger class="data-[state=active]:shadow-none" value="user">User</Tabs.Trigger>
           <Tabs.Trigger class="data-[state=active]:shadow-none" value="feeds">Feeds</Tabs.Trigger>
-          <Tabs.Trigger class="data-[state=active]:shadow-none" value="bookmarks"
-            >About</Tabs.Trigger
-          >
+          <Tabs.Trigger class="data-[state=active]:shadow-none" value="about">About</Tabs.Trigger>
         </Tabs.List>
       </div>
       <Tabs.Content value="user" class="overflow-y-scroll px-4 pr-2  max-h-[calc(100vh_-_168px)]">
@@ -36,10 +46,7 @@
       <Tabs.Content value="feeds" class="overflow-y-scroll px-4 pr-2  max-h-[calc(100vh_-_168px)]">
         <FeedsSection />
       </Tabs.Content>
-      <Tabs.Content
-        value="bookmarks"
-        class="overflow-y-scroll px-4 pr-2  max-h-[calc(100vh_-_168px)]"
-      >
+      <Tabs.Content value="about" class="overflow-y-scroll px-4 pr-2  max-h-[calc(100vh_-_168px)]">
         <AboutSection />
       </Tabs.Content>
     </Tabs.Root>
