7a46f9d966daebd510cfb99acc1ea7f04d9715c7
ndom91 <yo@ndo.dev>
2024-03-18 18:33:56 +0100

    feat: add flake based devshell

--- flake.nix
+++ flake.nix
@@ -1,43 +1,27 @@
 {
-  # Example from: https://johns.codes/blog/building-typescript-node-apps-with-nix
-  description = "Sample Nix ts-node build";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
-    let
-      dream2nixOutputs = dream2nix.lib.makeFlakeOutputs {
-        systems = flake-utils.lib.defaultSystems;
-        config.projectRoot = ./.;
-        source = gitignore.lib.gitignoreSource ./.;
-      };
-      customOutput = flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          # the dream2nix output for this system
-          app = dream2nixOutputs.packages."${system}".example-node-nix;
-        in
-        with pkgs; {
-          packages.docker = dockerTools.buildImage {
-            name = app.packageName;
-            copyToRoot = pkgs.buildEnv {
-              name = app.packageName;
-              paths = [ app ];
-              pathsToLink = [ "/bin" "/lib" ];
-            };
-            # This ensures symlinks to directories are preserved in the image
-            keepContentsDirlinks = true;
-            config = { Cmd = [ "/bin/ts-node-nix" ]; };
-          };
-        });
-      # deep merge outputs together
-    in
-    nixpkgs.lib.recursiveUpdate dream2nixOutputs customOutput;
-}
 
+  outputs = { nixpkgs, ... }:
+    let
+      genSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      devShells = genSystems (system: {
+        default = pkgsFor.${system}.mkShell {
+          packages = with pkgsFor.${system}; [
+            nodejs_20
+            nodePackages.pnpm
+            bun
+            openssl
+            vips # for sharp
+            pkg-config
+            prisma-engines
+            stdenv.cc.cc
+          ];
+        };
+      });
+    };
+}
