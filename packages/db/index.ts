81f69894dbd2e1659b32492fb19c92b7e8192c55
ndom91 <yo@ndo.dev>
2024-03-23 19:12:01 +0100

    fix: get working with webapp!!!

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,44 +1,43 @@
-import { PrismaClient } from "@prisma/client";
-import type { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
+import type { Prisma } from "@prisma/client"
 
-export * from "@prisma/client"
+// export * from "@prisma/client"
+export { PrismaClient, Prisma }
 export * as prismaZod from "./types/index.js"
 
 // TODO: any for now since 'PrismaClient'
 // const globalForPrisma = globalThis as { prisma?: any };
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// const createPrismaClient = () => {
+//   const db = new PrismaClient().$extends({
+//     name: "findManyAndCount",
+//     model: {
+//       $allModels: {
+//         findManyAndCount<Model, Args>(
+//           this: Model,
+//           args: Prisma.Exact<Args, Prisma.Args<Model, "findMany">>,
+//         ): Promise<[Prisma.Result<Model, Args, "findMany">, number]> {
+//           return db.$transaction([
+//             (this as any).findMany(args),
+//             (this as any).count({ where: (args as any).where }),
+//           ]) as any
+//         },
+//       },
+//     },
+//   })
+//   return db
+// }
+// export const db = globalForPrisma.prisma ?? PrismaClient
+// export const db = createPrismaClient()
 //
-// export type IExtendedPrismaClient = Omit<PrismaClient, "$on">;
-
-// export const prisma = globalThis.prisma || 
-const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient | undefined;
-};
-
-const createPrismaClient = () => {
-  const db = new PrismaClient().$extends({
-    name: "findManyAndCount",
-    model: {
-      $allModels: {
-        findManyAndCount<Model, Args>(
-          this: Model,
-          args: Prisma.Exact<Args, Prisma.Args<Model, "findMany">>,
-        ): Promise<[Prisma.Result<Model, Args, "findMany">, number]> {
-          return db.$transaction([
-            (this as any).findMany(args),
-            (this as any).count({ where: (args as any).where }),
-          ]) as any
-        },
-      },
-    },
-  })
-  return db
-}
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-declare global {
-  // We need `var` to declare a global variable in TypeScript
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   // We need `var` to declare a global variable in TypeScript
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined
+// }
 
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
