30755608019716f588a5bd6736625fe0ba33e256
ndom91 <yo@ndo.dev>
2024-04-04 17:33:52 +0200

    feat: new empty state for bookmark page

--- apps/web/src/lib/components/EmptyState.svelte
+++ apps/web/src/lib/components/EmptyState.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-  import type { Snippet } from "svelte"
+  import { cn } from "$lib/utils/style"
   import EmptyIllustration from "$lib/assets/new.png"
   import Arrow from "$lib/assets/arrow.svg?raw"
+  import type { Snippet } from "svelte"
 
   const {
     showArrow = true,
     illustration,
+    class: className,
   }: {
     showArrow?: boolean
     illustration?: Snippet
+    class?: string
   } = $props()
 </script>
 
@@ -17,11 +20,11 @@
     {@html Arrow}
   </div>
 {/if}
-<div class="flex flex-col items-center mx-auto w-1/2">
+<div class={cn(className, "flex flex-col items-center mx-auto w-full md:w-1/2")}>
   {#if illustration}
     {@render illustration()}
   {:else}
     <img src={EmptyIllustration} alt="Empty" class="w-full max-w-xl grayscale dark:invert" />
   {/if}
-  <p class="mb-4 text-2xl font-light">Looks like there's nothing here!</p>
+  <p class="px-8 mb-4 text-3xl font-light text-center">Looks like there's nothing here!</p>
 </div>
