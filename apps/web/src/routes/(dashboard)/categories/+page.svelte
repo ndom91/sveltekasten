a642e8a00dec953aec502484aacc3e7fd4da89ae
ndom91 <yo@ndo.dev>
2024-05-06 21:02:27 +0200

    fix: cleanup category and tags page inputs

--- apps/web/src/routes/(dashboard)/categories/+page.svelte
+++ apps/web/src/routes/(dashboard)/categories/+page.svelte
@@ -76,6 +76,16 @@
 <main
   class="p-4 align-start overflow-y-scroll flex max-h-[calc(100vh_-_80px)] w-full flex-col justify-start gap-2"
 >
+  <form
+    method="post"
+    action="?/createCategory"
+    use:enhance={handleActionResults()}
+    class="flex gap-4 justify-start items-center py-4"
+  >
+    <Input class="w-60" placeholder="Name" id="name" name="name" type="text" />
+    <Input class="w-96" placeholder="Description" id="description" name="description" type="text" />
+    <Button variant="secondary" type="submit" class="w-24 focus:">Save</Button>
+  </form>
   <Table.Root {...$tableAttrs}>
     <Table.Header>
       {#each $headerRows as headerRow}
@@ -157,32 +167,4 @@
       {/each}
     </Table.Body>
   </Table.Root>
-  <form
-    method="post"
-    action="?/createCategory"
-    use:enhance={handleActionResults()}
-    class="flex gap-4 justify-start items-center"
-  >
-    <div class="flex gap-2">
-      <svg
-        class="size-6"
-        data-slot="icon"
-        fill="none"
-        stroke-width="1.5"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
-        ></path>
-      </svg>
-    </div>
-    <Input class="w-60" placeholder="Name" id="name" name="name" type="text" />
-    <Input class="w-96" placeholder="Description" id="description" name="description" type="text" />
-    <Button variant="secondary" type="submit" class="w-24">Save</Button>
-  </form>
 </main>
