3d5708da98a16b1fec95d840f8b21f8f81846f52
ndom91 <yo@ndo.dev>
2024-05-17 14:40:33 +0200

    fix(web): add logging color/style

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -3,6 +3,21 @@ import type { Handle } from "@sveltejs/kit"
 import { handle as handleAuth, providerMap } from "./auth"
 import { dev } from "$app/environment"
 
+function getStatusColor(statusCode: string): string {
+  switch (true) {
+    case /^4\d{2}$/.test(statusCode):
+      return "\x1B[31m"
+    case /^5\d{2}$/.test(statusCode):
+      return "\x1B[31m"
+    case /^3\d{2}$/.test(statusCode):
+      return "\x1B[33m"
+    case /^2\d{2}$/.test(statusCode):
+      return "\x1B[32m"
+    default:
+      return "green"
+  }
+}
+
 const logger: Handle = async ({ event, resolve }) => {
   if (!dev) {
     return resolve(event)
@@ -12,8 +27,10 @@ const logger: Handle = async ({ event, resolve }) => {
   // Wait on response, run other hooks and load
   const response = await resolve(event)
 
+  const statusColor = getStatusColor(`${response.status}`)
+
   console.log(
-    `${response.status} ${event.request.method} ${event.url.pathname} (${Date.now() - start_time}ms)`,
+    `${statusColor}${response.status}\x1B[0m ${event.request.method} \x1B[1m${event.url.pathname}\x1B[0m (\x1B[90m${Date.now() - start_time}ms\x1B[0m)`,
   )
 
   return response
