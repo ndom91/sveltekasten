7e6bca557232b3371d818af257f376864729eedc
ndom91 <yo@ndo.dev>
2024-05-24 18:37:07 +0200

    fix: comment out db session code

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { getCookie } from "hono/cookie"
 import { HTTPException } from "hono/http-exception"
 import type { Context } from "hono"
-import { db } from "../plugins/prisma.js"
+// import { db } from "../plugins/prisma.js"
 import debugFactory from "./log.js"
 import { verifyJwt } from "./jwt.js"
 
@@ -9,20 +9,20 @@ const debug = debugFactory("backend:auth")
 
 const AUTH_SESSION_STRATEGY: "database" | "jwt" = "jwt"
 
-const getSession = async (sessionToken: string) => {
-  debug("Getting session", { sessionToken })
-  return db.session.findFirst({
-    where: {
-      sessionToken,
-      expires: {
-        gte: new Date(),
-      },
-    },
-    select: {
-      userId: true,
-    },
-  })
-}
+// const getSession = async (sessionToken: string) => {
+//  debug("Getting session", { sessionToken })
+//  return db.session.findFirst({
+//    where: {
+//      sessionToken,
+//      expires: {
+//        gte: new Date(),
+//      },
+//    },
+//    select: {
+//      userId: true,
+//    },
+//  })
+// }
 
 export async function getUserId(c: Context) {
   try {
@@ -38,10 +38,10 @@ export async function getUserId(c: Context) {
       const decodedJwt = await verifyJwt(cookieValue)
       debug("Parsing userId (jwt)", { id: decodedJwt?.sub })
       return decodedJwt?.sub
-    } else if (AUTH_SESSION_STRATEGY === "database") {
-      const session = await getSession(cookieValue)
-      debug("Parsing userId (db)", { id: session?.userId })
-      return session?.userId
+    // } else if (AUTH_SESSION_STRATEGY === "database") {
+    //  const session = await getSession(cookieValue)
+    //  debug("Parsing userId (db)", { id: session?.userId })
+    //  return session?.userId
     }
   } catch (error) {
     console.error(error)
