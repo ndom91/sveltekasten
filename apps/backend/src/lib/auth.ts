9fd783a5e17e00742be1175857f48e66ff6575b5
ndom91 <yo@ndo.dev>
2024-05-25 16:07:05 +0200

    fix: cleanup backend/auth.ts

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -1,28 +1,10 @@
 import { getCookie } from "hono/cookie"
 import { HTTPException } from "hono/http-exception"
 import type { Context } from "hono"
-// import { db } from "../plugins/prisma.js"
 import debugFactory from "./log.js"
 import { verifyJwt } from "./jwt.js"
 
 const debug = debugFactory("backend:auth")
-
-const AUTH_SESSION_STRATEGY: "database" | "jwt" = "jwt"
-
-// const getSession = async (sessionToken: string) => {
-//  debug("Getting session", { sessionToken })
-//  return db.session.findFirst({
-//    where: {
-//      sessionToken,
-//      expires: {
-//        gte: new Date(),
-//      },
-//    },
-//    select: {
-//      userId: true,
-//    },
-//  })
-// }
 
 export async function getUserId(c: Context) {
   try {
@@ -34,15 +16,9 @@ export async function getUserId(c: Context) {
 
     debug("Parsing cookies", { cookieName, cookieValue })
 
-    if (AUTH_SESSION_STRATEGY === "jwt") {
-      const decodedJwt = await verifyJwt(cookieValue)
-      debug("Parsing userId (jwt)", { id: decodedJwt?.sub })
-      return decodedJwt?.sub
-    // } else if (AUTH_SESSION_STRATEGY === "database") {
-    //  const session = await getSession(cookieValue)
-    //  debug("Parsing userId (db)", { id: session?.userId })
-    //  return session?.userId
-    }
+    const decodedJwt = await verifyJwt(cookieValue)
+    debug("Decoded JWT.sub", { id: decodedJwt?.sub })
+    return decodedJwt?.sub
   } catch (error) {
     console.error(error)
     throw new HTTPException(401, { message: "Unauthorized" })
