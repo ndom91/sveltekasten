0c0cfb3f762140fb07672ecd30910ad51a20333c
ndom91 <yo@ndo.dev>
2024-06-01 21:36:43 +0200

    fix: add exit btn to audioplayer

--- apps/web/src/lib/components/audio-player/AudioPlayer.svelte
+++ apps/web/src/lib/components/audio-player/AudioPlayer.svelte
@@ -232,5 +232,22 @@
       >
       <div class="text-[0.65rem]">{playbackRate.toFixed(1)}</div>
     </button>
+
+    {#if windowWidth < 768}
+      <button onclick={() => (ui.textToSpeechAudioBlob = "")}>
+        <svg
+          class="size-6"
+          data-slot="icon"
+          fill="none"
+          stroke-width="1.5"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
+        </svg>
+      </button>
+    {/if}
   </div>
 </div>
