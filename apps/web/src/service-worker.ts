eaa4c7a002238fd9b1dd6ce7605e0afa118401e3
ndom91 <yo@ndo.dev>
2024-06-23 19:30:40 +0200

    fix: try another fetch event ahndler

--- apps/web/src/service-worker.ts
+++ apps/web/src/service-worker.ts
@@ -12,52 +12,91 @@ self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting()
 })
 
+// Special fetch handler for song file sharing.
 self.addEventListener("fetch", (event: FetchEvent) => {
-  // Regular requests not related to Web Share Target.
-  if (event.request.method !== "POST") {
-    event.respondWith(fetch(event.request))
+  const url = new URL(event.request.url)
+
+  if (event.request.method !== "POST" || !url.pathname.includes("/api/v1/bookmarks/share")) {
     return
   }
 
-  if (!event.clientId) return
+  // Immediately redirect to the start URL, there's nothing to see here.
+  event.respondWith(Response.redirect("./"))
 
-  if (
-    event.request.method === "POST" &&
-    new URL(event.request.url).pathname === "/api/v1/bookmarks/share"
-  ) {
-    event.respondWith(
-      (async () => {
-        const formData = await event.request.formData()
-        const url = formData.get("url") || ""
-        // TODO: Get userId
-        const userId = "abc123"
-        // const body = await event.request.json()
-        // const responseUrl = await fetch("/api/v1/bookmarks")
+  event.waitUntil(
+    (async function () {
+      const formData = await event.request.formData()
+      const url = formData.get("url") || ""
+      // TODO: Get userId
+      const userId = "clu6qepua0000scqbkr2t0uks"
 
-        await fetch("/api/v1/bookmarks", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
+      await fetch("/api/v1/bookmarks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([
+          {
+            url,
+            userId,
           },
-          body: JSON.stringify([
-            {
-              url,
-              userId,
-            },
-          ]),
-        })
-        const client = await self.clients.get(event.clientId)
-        client?.postMessage({
-          msg: "Got your share!",
-          url,
-        })
-        // TODO: postMessage() back to foreground to alert success or fail
-        // https://developer.chrome.com/docs/capabilities/web-apis/web-share-target
-        return Response.redirect("/", 303)
-      })(),
-    )
-  }
+        ]),
+      })
+      const client = await self.clients.get(event.clientId)
+      client?.postMessage({
+        msg: "Got your share!",
+        url,
+      })
+    })(),
+  )
 })
+
+// self.addEventListener("fetch", (event: FetchEvent) => {
+//   // Regular requests not related to Web Share Target.
+//   if (event.request.method !== "POST") {
+//     event.respondWith(fetch(event.request))
+//     return
+//   }
+//
+//   if (!event.clientId) return
+//
+//   if (
+//     event.request.method === "POST" &&
+//     new URL(event.request.url).pathname === "/api/v1/bookmarks/share"
+//   ) {
+//     event.respondWith(
+//       (async () => {
+//         const formData = await event.request.formData()
+//         const url = formData.get("url") || ""
+//         // TODO: Get userId
+//         const userId = "abc123"
+//         // const body = await event.request.json()
+//         // const responseUrl = await fetch("/api/v1/bookmarks")
+//
+//         await fetch("/api/v1/bookmarks", {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify([
+//             {
+//               url,
+//               userId,
+//             },
+//           ]),
+//         })
+//         const client = await self.clients.get(event.clientId)
+//         client?.postMessage({
+//           msg: "Got your share!",
+//           url,
+//         })
+//         // TODO: postMessage() back to foreground to alert success or fail
+//         // https://developer.chrome.com/docs/capabilities/web-apis/web-share-target
+//         return Response.redirect("/", 303)
+//       })(),
+//     )
+//   }
+// })
 
 // async function messageClient(clientId) {
 //   const client = await clients.get(clientId)
