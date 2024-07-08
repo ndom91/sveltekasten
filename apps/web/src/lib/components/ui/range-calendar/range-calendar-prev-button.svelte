6df563c9f3d9829773d2990279e135185e6013d9
ndom91 <yo@ndo.dev>
2024-07-08 22:53:26 +0200

    fix: rm all references to lucide

--- apps/web/src/lib/components/ui/range-calendar/range-calendar-prev-button.svelte
+++ apps/web/src/lib/components/ui/range-calendar/range-calendar-prev-button.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
-	import { RangeCalendar as RangeCalendarPrimitive } from "bits-ui";
-	import ChevronLeft from "lucide-svelte/icons/chevron-left";
-	import { buttonVariants } from "$lib/components/ui/button/index.js";
-	import { cn } from "$lib/utils/style";
+  import { RangeCalendar as RangeCalendarPrimitive } from "bits-ui"
+  import { buttonVariants } from "$lib/components/ui/button/index.js"
+  import { cn } from "$lib/utils/style"
 
-	type $$Props = RangeCalendarPrimitive.PrevButtonProps;
-	type $$Events = RangeCalendarPrimitive.PrevButtonEvents;
+  type $$Props = RangeCalendarPrimitive.PrevButtonProps
+  type $$Events = RangeCalendarPrimitive.PrevButtonEvents
 
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+  let className: $$Props["class"] = undefined
+  export { className as class }
 </script>
 
 <RangeCalendarPrimitive.PrevButton
-	on:click
-	class={cn(
-		buttonVariants({ variant: "outline" }),
-		"h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
-		className
-	)}
-	{...$$restProps}
-	let:builder
+  on:click
+  class={cn(
+    buttonVariants({ variant: "outline" }),
+    "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
+    className,
+  )}
+  {...$$restProps}
+  let:builder
 >
-	<slot {builder}>
-		<ChevronLeft class="h-4 w-4" />
-	</slot>
+  <slot {builder}>
+    <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"
+      ><rect width="256" height="256" fill="none" /><polyline
+        points="160 208 80 128 160 48"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      /></svg
+    >
+  </slot>
 </RangeCalendarPrimitive.PrevButton>
