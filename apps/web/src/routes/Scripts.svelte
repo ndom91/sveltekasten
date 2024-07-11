2b9e22e1ee9bd21ff34dae2efb1c260be409a1d9
ndom91 <yo@ndo.dev>
2024-07-11 13:20:26 +0200

    fix: url state

--- apps/web/src/routes/Scripts.svelte
+++ apps/web/src/routes/Scripts.svelte
@@ -6,11 +6,12 @@
   import * as Swetrix from "swetrix"
   import { env } from "$env/dynamic/public"
 
-  let url = $page.url.pathname
-  if ($page.url.searchParams.toString() !== "") {
-    url += `?${$page.url.searchParams.toString()}`
-  }
+  let url = $state("")
   onMount(() => {
+    url = $page.url.pathname
+    if ($page.url.searchParams.toString() !== "") {
+      url += `?${$page.url.searchParams.toString()}`
+    }
     if (!dev && $page.url.hostname === "dev.briefkastenhq.com") {
       Swetrix.init(env.PUBLIC_SWETRIX_PROJECT, { apiURL: env.PUBLIC_SWETRIX_API_HOST })
 
