f0aed91c12851911bd447667f830fe8142de590c
ndom91 <yo@ndo.dev>
2024-07-11 13:14:31 +0200

    fix: watch url and trigger pageViews

--- apps/web/src/routes/Scripts.svelte
+++ apps/web/src/routes/Scripts.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
   import { onMount } from "svelte"
+  import { watch } from "runed"
   import { page } from "$app/stores"
-  import { dev } from "$app/environment"
+  import { browser, dev } from "$app/environment"
   import * as Swetrix from "swetrix"
   import { env } from "$env/dynamic/public"
 
+  let url = $page.url.pathname
+  if ($page.url.searchParams.toString() !== "") {
+    url += `?${$page.url.searchParams.toString()}`
+  }
   onMount(() => {
     if (!dev && $page.url.hostname === "dev.briefkastenhq.com") {
       Swetrix.init(env.PUBLIC_SWETRIX_PROJECT, { apiURL: env.PUBLIC_SWETRIX_API_HOST })
 
-      let url = $page.url.pathname
-      if ($page.url.searchParams.toString() !== "") {
-        url += `?${$page.url.searchParams.toString()}`
-      }
       Swetrix.trackPageview(url)
     }
   })
+
+  watch(
+    () => url,
+    () => {
+      browser && Swetrix.trackPageview(url)
+    },
+  )
 </script>
