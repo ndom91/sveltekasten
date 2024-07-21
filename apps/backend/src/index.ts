088ecf1fd10a44e99052d37c42e7ab618a3c15e9
ndom91 <yo@ndo.dev>
2024-07-21 20:58:42 +0200

    feat: add ratelimiter to backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono"
 import { logger } from "hono/logger"
 import { format } from "@formkit/tempo"
 import { prettyJSON } from "hono/pretty-json"
+import { rateLimiter } from "hono-rate-limiter";
 import { type HttpBindings, serve } from "@hono/node-server"
 
 import { updateJob } from "./jobs/cron-update.js"
@@ -9,11 +10,24 @@ import bookmark from "./routes/bookmark/index.js"
 import feed from "./routes/feed/index.js"
 import root from "./routes/root.js"
 
+const limiter = rateLimiter({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  limit: 5, // Limit each IP to 100 requests per `window`
+  standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  keyGenerator: (c) => {
+    const a = c.req.header('CF-Connecting-IP') || c.req.header('X-Forwarded-For') || c.req.header('origin') || '127.0.0.1' // Method to generate custom identifiers for clients.
+    console.log('clientIp', a)
+    return a
+  }
+});
+
 const app = new Hono<{ Bindings: HttpBindings }>({ strict: false }).basePath(
   "/worker/v1",
 )
+
 app.use(logger())
 app.use(prettyJSON())
+app.use(limiter)
 
 app.route("/bookmark", bookmark)
 app.route("/feed", feed)
