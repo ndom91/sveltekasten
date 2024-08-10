5924b204dadeb00f7ca36a35da3f0389e96765fb
ndom91 <yo@ndo.dev>
2024-08-10 15:04:05 +0200

    fix: eslint tailwind.config.js

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,6 +6,7 @@ const config = {
   darkMode: ["class"],
   content: ["./src/**/*.{html,js,svelte,ts}"],
   safelist: ["dark"],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("@tailwindcss/typography")],
   theme: {
     container: {
@@ -16,11 +17,11 @@ const config = {
       },
     },
     boxShadow: {
-      "xs": StaticShadows["--shadow-1"],
-      "sm": StaticShadows["--shadow-2"],
-      "md": StaticShadows["--shadow-3"],
-      "lg": StaticShadows["--shadow-4"],
-      "xl": StaticShadows["--shadow-5"],
+      xs: StaticShadows["--shadow-1"],
+      sm: StaticShadows["--shadow-2"],
+      md: StaticShadows["--shadow-3"],
+      lg: StaticShadows["--shadow-4"],
+      xl: StaticShadows["--shadow-5"],
       "2xl": StaticShadows["--shadow-6"],
       "3xl": [
         "0px 0px 7.6px rgba(0, 0, 0, 0.046)",
