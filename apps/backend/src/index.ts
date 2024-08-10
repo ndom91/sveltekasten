0c105c6fff5c03ea25135fce4c03880ea5f2f2d0
ndom91 <yo@ndo.dev>
2024-08-10 19:35:32 +0200

    feat: implement ipx image proxy + lru cache

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,51 +5,33 @@ import { logger } from "hono/logger"
 import { prettyJSON } from "hono/pretty-json"
 import { rateLimiter } from "hono-rate-limiter";
 
-import {
-  createIPX,
-  createIPXWebServer,
-  ipxFSStorage,
-  ipxHttpStorage,
-} from 'ipx';
 import { updateJob } from "./jobs/cron-update.js"
+import { imageProxyHandler } from "./lib/imageProxy.js"
 import bookmark from "./routes/bookmark/index.js"
 import feed from "./routes/feed/index.js"
-import root from "./routes/root.js"
-// import { ipxHandler } from "./lib/imageProxy.js"
 
+const img = new Hono<{ Bindings: HttpBindings }>()
+img.use("/*", imageProxyHandler)
 
-const ipx = createIPX({
-  storage: ipxFSStorage(),
-  httpStorage: ipxHttpStorage({
-    allowAllDomains: true,
-  }),
-});
-
-const limiter = rateLimiter({
-  windowMs: 1 * 60 * 1000, // 5 minutes
-  limit: 5, // Limit each IP to 100 requests per `window`
-  standardHeaders: "draft-6",
-  keyGenerator: (c) => {
-    return c.req.header('X-Forwarded-For') || c.req.header('X-Real-IP') || c.req.header('CF-Connecting-IP') || '127.0.0.1'
-  }
-});
-
-const app = new Hono<{ Bindings: HttpBindings }>().basePath(
-  "/worker/v1",
-)
+const app = new Hono<{ Bindings: HttpBindings }>()
+app.route('/img', img)
 
 app.use(logger())
 app.use(prettyJSON())
 if (process.env.NODE_ENV === "production") {
-  app.use(limiter)
+  app.use(rateLimiter({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    limit: 25,
+    standardHeaders: "draft-6",
+    keyGenerator: (c) => {
+      return c.req.header('X-Forwarded-For') || c.req.header('X-Real-IP') || c.req.header('CF-Connecting-IP') || '127.0.0.1'
+    }
+  }))
 }
-app.use("/*", c => createIPXWebServer(ipx)(c.req.raw))
 
-app.route("/bookmark", bookmark)
-app.route("/feed", feed)
-app.route("/", root)
-
-app.get("*", c => c.notFound())
+const BASE_PATH = "/worker/v1"
+app.route(`${BASE_PATH}/bookmark`, bookmark)
+app.route(`${BASE_PATH}/feed`, feed)
 
 const port = process.env.PORT ? Number.parseInt(process.env.PORT) : 8000
 
--- apps/backend/src/lib/imageProxy.ts
+++ apps/backend/src/lib/imageProxy.ts
@@ -1,18 +1,46 @@
+import { type Context } from "hono"
 import {
   createIPX,
   createIPXWebServer,
   ipxFSStorage,
   ipxHttpStorage,
 } from 'ipx';
+import { LRUCache } from "lru-cache";
 
 const ipx = createIPX({
-  storage: ipxFSStorage({ dir: './public/' }),
+  storage: ipxFSStorage(),
   httpStorage: ipxHttpStorage({
     allowAllDomains: true,
-    domains: ['images.unsplash.com']
   }),
 });
 
-export const ipxHandler = createIPXWebServer(ipx)
+const cache = new LRUCache({
+  max: 200,
+});
 
-// const app = new Hono().use('/', (c) => createIPXWebServer(ipx)(c.req.raw));
+export const imageProxyHandler = async (c: Context) => {
+  const targetUrl = c.req.raw.url.replace(/\/img/, "")
+
+  const cachedResponse = cache.get(targetUrl)
+  if (cachedResponse) {
+    return new Response(cachedResponse as Blob, {
+      status: 200,
+      headers: {
+        "x-cache": "HIT",
+        "Content-Type": "image/jpeg",
+        'cache-control': 'max-age=31536000, public, s-maxage=31536000',
+      },
+    })
+  }
+
+  const response = await createIPXWebServer(ipx)(new Request(targetUrl))
+
+  const clonedResponse = response.clone()
+  const data = await clonedResponse.blob()
+  cache.set(targetUrl, data)
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText
+  })
+}
