85ca86efa3ca2dc966d0539c643bd917bd1933ee
ndom91 <yo@ndo.dev>
2024-08-11 21:26:17 +0200

    fix: improve picsum placeholder image loading

--- apps/web/src/lib/components/bookmark-row/BookmarkRow.svelte
+++ apps/web/src/lib/components/bookmark-row/BookmarkRow.svelte
@@ -53,7 +53,7 @@
     if (bookmark.image) {
       return `${PUBLIC_WORKER_URL}/img/s_260x144/${bookmark.image}`
     } else {
-      return `${PUBLIC_WORKER_URL}/img/_/https://source.unsplash.com/random/260x144?sig=${bookmark.url}`
+      return `${PUBLIC_WORKER_URL}/img/_/https://picsum.photos/seed/${btoa(bookmark.url).substring(0, 16)}/256/144.webp`
     }
   })
 
