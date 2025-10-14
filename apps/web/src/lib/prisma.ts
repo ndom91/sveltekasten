b55547c8e38e5d85095e1fd7c5817f89013025cf
ndom91 <yo@ndo.dev>
2025-10-14 16:34:17 +0200

    Update Prisma files

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -1,8 +1,7 @@
-import { PrismaPg } from "@prisma/adapter-pg"
-import { Prisma } from "@prisma/client"
-import { PrismaClient } from "../prisma-client"
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Prisma, PrismaClient } from "../prisma-client/client.js";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 // TODO: Reimplement singleton for prod
 const db = new PrismaClient({ adapter }).$extends({
@@ -16,15 +15,15 @@ const db = new PrismaClient({ adapter }).$extends({
         this: Model,
         args: Prisma.Args<Model, "findMany">,
       ): Promise<[Prisma.Result<Model, Args, "findMany">, number]> {
-        const context = Prisma.getExtensionContext(this)
+        const context = Prisma.getExtensionContext(this);
 
         return db.$transaction([
           (context as any).findMany(args),
           (context as any).count({ where: args.where }),
-        ]) as Promise<[Prisma.Result<Model, Args, "findMany">, number]>
+        ]) as Promise<[Prisma.Result<Model, Args, "findMany">, number]>;
       },
     },
   },
-})
+});
 
-export { db }
+export { db };
